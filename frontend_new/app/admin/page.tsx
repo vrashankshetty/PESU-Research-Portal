ff9413f4c8838a9e313a0024846dd06e32dec800
Vrashank Shetty <vrashankshetty.s@gmail.com>
2025-02-03 20:39:19 +0530

    fix

--- frontend_new/app/admin/page.tsx
+++ frontend_new/app/admin/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Book, Microscope, User } from "lucide-react";
+import { Book, Microscope,Users } from "lucide-react";
 
 export default function Home() {
   return (
